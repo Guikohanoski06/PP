1e4f4ed61ed1eaae783417161b788ea6c3236f9c
GUILHERME KOHANOSKI <02458534058@senacrs.edu.br>
2024-10-25 11:29:31 -0300

    arrumando commit

--- login e cadastro/src/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/userController');
-
-// Rota para buscar todos os psicólogos
-router.get('/psychologists', userController.getPsychologists);
-
-module.exports = router;
--- loginCadastro_profissional/src/routes/userRouter.js
+++
@@ -1,7 +0,0 @@
-const {Router} = require('express');
-const router = Router();
-const { storeUser } = require('../controller/userController');
-
-router.post('/user/create', storeUser);
-
-module.exports = router;
